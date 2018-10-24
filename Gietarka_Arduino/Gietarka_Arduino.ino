16130b71b13431ed358391c66d0c03bf5d4e0a10
lykoszczan <lykoszczan@gmail.com>
2018-10-24 19:23:51 +0200

    Dodaj pliki projektów.

---
+++ Gietarka_Arduino/Gietarka_Arduino.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Gietarka_Arduino.ino
+ Created:	24.10.2018 19:23:46
+ Author:	lykos
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
